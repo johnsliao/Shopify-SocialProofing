7c742462a2ae11c21e060e0ffa78c56ed735a9c6
johnsliao <john@johnliao.org>
2017-11-14 13:05:13 -0500

    Add unit tests

--- app/decorators.py
+++ app/decorators.py
@@ -10,7 +10,6 @@ logger = logging.getLogger(__name__)
 def shop_login_required(func):
     def wrapper(request, *args, **kwargs):
         if not hasattr(request, 'session') or 'shopify' not in request.session:
-            print(request.__dict__)
             request.session['return_to'] = request.get_full_path()
             return redirect(reverse('install'))
         return func(request, *args, **kwargs)
