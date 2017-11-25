d74efaf9afb968bfb3eb9b146f9be9728867aba6
johnsliao <john@johnliao.org>
2017-11-25 15:19:13 -0500

    Clean up populate defaults

--- app/models.py
+++ app/models.py
@@ -50,7 +50,7 @@ class Orders(models.Model):
 class Modal(models.Model):
     store = models.ForeignKey(Store, on_delete=models.CASCADE)
 
-    location = models.CharField(max_length=200)
+    location = models.CharField(max_length=200, default='bottom-right')
     color_hue = models.FloatField(default=83.28)
     color_saturation = models.FloatField(default=0.306)
     color_brightness = models.FloatField(default=0.487)
--- app/utils.py
+++ app/utils.py
@@ -65,20 +65,10 @@ def populate_default_settings(store_name):
     Populate db with default settings
     """
 
-    default_look_back = 24  # Look back in hours
-    default_duration = 5  # Modal popup duration
-    default_location = 'bottom-right'  # Default modal location
-    default_color = '#4286f4'  # Default modal color
-
     try:
         store = Store.objects.get(store_name=store_name)
     except Store.DoesNotExists:
         store = store.objects.create(store_name=store_name)
 
-    StoreSettings.objects.create(look_back=default_look_back, store=store)
-
-    Modal.objects.create(store=store,
-                         location=default_location,
-                         color=default_color,
-                         duration=default_duration,
-                         )
+    StoreSettings.objects.create(store=store)
+    Modal.objects.create(store=store)
